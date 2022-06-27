c75bec730cbf1fe13d06058b98ab3f8c3e03d046
Juniperd <juniperldempsey@GMAIL.COM>
2022-06-27 13:11:14 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -130,16 +130,15 @@ let footer = $(`
           </div>
 
 
-
         <div class="col-lg-6 col-md-12 mb-4 mb-md-0 form-comtainer">
           <div class="form-style-6">
              <div class="form-header">
                 <h6 class="display">Get in Touch</h6>
               </div>
-                <form name="form1" action="hhttps://formcarry.com/s/PLfbg4UXmLq" method="POST" accept-charset="UTF-8"/>
+                <form name="form1" action="https://formcarry.com/s/PLfbg4UXmLq" method="POST" accept-charset="UTF-8" >
                   <input id="name" type="text" name="name" placeholder="Your Name" required/>
-                  <input id="email" type="email" name="email" placeholder="Your Email" required/>                  
-                  <textarea id="textArea" name="message" placeholder="A dope message" required></textarea>
+                  <input id="email" type="email" name="email" placeholder="Email Address" required/>                  
+                  <textarea id="textArea" name="message" placeholder="Type your Message" required></textarea>
               
                   <div id="main">
                     <button id="lnch" type="button" value="Send" >Send</button>
